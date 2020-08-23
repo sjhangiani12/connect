e3af931c3c0f52d7f0f9efde17aa025938c2e4d6
sjhangiani12 <sjhangiani12@users.noreply.github.com>
2020-08-23 05:08:31 -0700

    added updates

--- airtable_manager.py
+++ airtable_manager.py
@@ -6,79 +6,58 @@ from datetime import datetime
 
 
 
-airtable_obj = Airtable(base_key=airtable_base_key, table_name='contacts', api_key=airtable_api_key)
+airtable_obj = Airtable(base_key=airtable_base_key, table_name="contacts", api_key=airtable_api_key)
 
 
 def get_friend():
-    for page in airtable_obj.get_iter(max_records=1, view='priority_queue'):
+    for page in airtable_obj.get_iter(max_records=1, view="priority_queue"):
         for record in page:
             user_records = record
             
-    user_id = user_records['id']
+    user_id = user_records["id"]
     friend_id = user_id
-    friend_name = user_records['fields']['name']
-    friend_source = user_records['fields']['source']
+    friend_name = user_records["fields"]["name"]
+    friend_source = user_records["fields"]["source"]
         
     return friend_id, friend_name, friend_source
 
 def update_considering(friend_id, indicator):
-    update_dict = {'in_consideration': indicator}
+    update_dict = {"in_consideration": indicator}
     airtable_obj.update(friend_id, update_dict)
-    print('marked as considering')
+    print("marked as considering")
     
 
 def get_considered():
-    for page in airtable_obj.get_iter(max_records=1, view='in_consideration'):
+    for page in airtable_obj.get_iter(max_records=1, view="in_consideration"):
         for record in page:
             user_records = record
             
-    user_id = user_records['id']
+    user_id = user_records["id"]
     friend_id = user_id
-    friend_name = user_records['fields']['name']
-    friend_source = user_records['fields']['source']
+    friend_name = user_records["fields"]["name"]
+    friend_source = user_records["fields"]["source"]
 
     return friend_id, friend_name, friend_source
 
 
 def mark_as_skipped(friend_id):
-    update_dict = {'last_skipped': datetime.today().strftime('%Y-%m-%d')}
+    update_dict = {"last_skipped": datetime.today().strftime("%Y-%m-%d")}
     airtable_obj.update(friend_id, update_dict)
-    print('marked as skip')
+    print("marked as skip")
 
 
 def mute_person(friend_id):
-    update_dict = {'muted': True}    
+    update_dict = {"muted": True}    
     airtable_obj.update(friend_id, update_dict)
-    print('muted')    
+    print("muted")    
 
 
 # did you talk to this person 
 #   - get some response and input that as True for False
 def mark_as_talked(friend_id, follow_up_val):
-    update_dict = {'follow_up': follow_up_val}
+    update_dict = {"follow_up": follow_up_val}
     if follow_up_val:
-        update_dict = {'follow_up': follow_up_val,
-                'last_contact': datetime.today().strftime('%Y-%m-%d')}
+        update_dict = {"follow_up": follow_up_val,
+                "last_contact": datetime.today().strftime("%Y-%m-%d")}
     airtable_obj.update(friend_id, update_dict)
-    print('marked date of convo')
-
-  
-# user flow: 
-
-# at 6pm:
-# do you want to talk to __ today?  -- get_friend()
-    # yes
-        # follow up text 2 hours later at 8pm -- did you talk to __ today? 
-            # yes
-                # update user with current date for last talked
-            # no
-                # do nothing 
-    # no
-        # mark skip date as today
-        # query next user 
-    # mute
-        # update user as muted
-        # query next user 
-    
-    
-    
+    print("marked date of convo")
--- server.py
+++ server.py
@@ -7,53 +7,53 @@ from airtable_manager import get_friend, update_considering, get_considered, mar
 
 app = Flask(__name__)
 
-# @app.route('/', methods=['GET'])
+# @app.route("/", methods=["GET"])
 # def ping():
-#     return 'Jarvis, start the engines.'
+#     return "Jarvis, start the engines."
 
-@app.route('/bot', methods=['POST'])
+@app.route("/bot", methods=["POST"])
 def bot():
-    incoming_msg = request.values.get('Body', '').lower()
+    incoming_msg = request.values.get("Body", "").lower()
     resp = MessagingResponse()
     msg = resp.message()
     responded = False
-    if 'hi' in incoming_msg:
+    if "hi" in incoming_msg:
         friend_id, friend_name, friend_source = get_friend()
-        msg_to_send = f'Do you want to speak to {friend_name}({friend_source})?'
+        msg_to_send = f"Do you want to speak to {friend_name}({friend_source})?"
         update_considering(friend_id, True)
         msg.body(msg_to_send)
         responded = True
-    elif 'yes' in incoming_msg:
+    elif "yes" in incoming_msg:
         friend_id, friend_name, friend_source = get_considered()
         update_considering(friend_id, False)
         mark_as_talked(friend_id, True)
         responded = True
-        msg_to_send = f'Enjoy your conversation with {friend_name}({friend_source})!'
+        msg_to_send = f"Enjoy your conversation with {friend_name}({friend_source})!"
         msg.body(msg_to_send)
-    if 'no' in incoming_msg:
+    if "no" in incoming_msg:
         friend_id, friend_name, friend_source = get_considered()
         update_considering(friend_id, False)
         mark_as_skipped(friend_id)
         friend_id, friend_name, friend_source = get_friend()
-        msg_to_send = f'Do you want to speak to {friend_name}({friend_source})?'
+        msg_to_send = f"Do you want to speak to {friend_name}({friend_source})?"
         update_considering(friend_id, True)
         msg.body(msg_to_send)
         responded = True
-    if 'mute' in incoming_msg:
+    if "mute" in incoming_msg:
         friend_id, friend_name, friend_source = get_considered()
         update_considering(friend_id, False)
         mute_person(friend_id)
         friend_id, friend_name, friend_source = get_friend()
-        msg_to_send = f'Do you want to speak to {friend_name}({friend_source})?'
+        msg_to_send = f"Do you want to speak to {friend_name}({friend_source})?"
         update_considering(friend_id, True)
         msg.body(msg_to_send)
         responded = True
     if not responded:
-        msg.body('There was an error, please try again.')
+        msg.body("There was an error, please try again.")
     return str(resp)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
 
-# serve(app, host='0.0.0.0', port=3000)
+# serve(app, host="0.0.0.0", port=3000)
